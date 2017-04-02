e429e34d170db698c53b47f9dd1943bef20862a9
CULETA-NB\CULETA <sculebras@hotmail.com>
2017-04-01 21:21:20 -0300

    Se habilita DatabaseInitialization

--- ClimaSistemaSolar.Servicio/Controllers/ValuesController.cs
+++ ClimaSistemaSolar.Servicio/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace ClimaSistemaSolar.Servicio.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return "value " + id;
         }
 
         // POST api/values
